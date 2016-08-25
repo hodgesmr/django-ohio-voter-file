ed7815523df2926ae4b6daf4a90658fa454240ca
Matt Hodges <hodgesmr1@gmail.com>
2016-08-25 08:19:56 -0400

    add DB settings

---
+++ ohiovoter/settings.py
@@ -0,0 +1,20 @@
+import os
+
+########## DATABASE CONFIGURATION
+DB_NAME = os.getenv('DB_NAME')
+HOST = os.getenv('DB_HOST')
+USER = os.getenv('DB_USER')
+PASS = os.getenv('DB_PASS')
+PORT = os.getenv('DB_PORT', 5432)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'HOST': DB_HOST,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'PORT': DB_PORT,
+    },
+
+}

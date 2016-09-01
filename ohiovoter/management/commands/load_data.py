fd3da07a4b288f862c396099d70290bf3cebedf9
Matt Hodges <hodgesmr1@gmail.com>
2016-08-31 21:46:42 -0400

    big freaking refactor

--- ohiovoter/management/commands/load_data.py
+++ ohiovoter/management/commands/load_data.py
@@ -4,8 +4,10 @@ from django.db import connection
 from datetime import datetime
 import hashlib
 from io import StringIO
+from multiprocessing.dummy import Pool as ThreadPool
 import os
 import tempfile
+import time
 import urllib.request
 import zipfile
 
@@ -17,94 +19,95 @@ from ohiovoter.models import Election, Voter
 
 COUNTIES = [
     'ADAMS',
-    # 'ALLEN',
-    # 'ASHLAND',
-    # 'ASHTABULA',
-    # 'ATHENS',
-    # 'AUGLAIZE',
-    # 'BELMONT',
-    # 'BROWN',
-    # 'BUTLER',
-    # 'CARROLL',
-    # 'CHAMPAIGN',
-    # 'CLARK',
-    # 'CLERMONT',
-    # 'CLINTON',
-    # 'COLUMBIANA',
-    # 'COSHOCTON',
-    # 'CRAWFORD',
-    # 'CUYAHOGA',
-    # 'DARKE',
-    # 'DEFIANCE',
-    # 'DELAWARE',
-    # 'ERIE',
-    # 'FAIRFIELD',
-    # 'FAYETTE',
-    # 'FRANKLIN',
-    # 'FULTON',
-    # 'GALLIA',
-    # 'GEAUGA',
-    # 'GREENE',
-    # 'GUERNSEY',
-    # 'HAMILTON',
-    # 'HANCOCK',
-    # 'HARDIN',
-    # 'HARRISON',
-    # 'HENRY',
-    # 'HIGHLAND',
-    # 'HOCKING',
-    # 'HOLMES',
-    # 'HURON',
-    # 'JACKSON',
-    # 'JEFFERSON',
-    # 'KNOX',
-    # 'LAKE',
-    # 'LAWRENCE',
-    # 'LICKING',
-    # 'LOGAN',
-    # 'LORAIN',
-    # 'LUCAS',
-    # 'MADISON',
-    # 'MAHONING',
-    # 'MARION',
-    # 'MEDINA',
-    # 'MEIGS',
-    # 'MERCER',
-    # 'MIAMI',
-    # 'MONROE',
-    # 'MONTGOMERY',
-    # 'MORGAN',
-    # 'MORROW',
-    # 'MUSKINGUM',
-    # 'NOBLE',
-    # 'OTTAWA',
-    # 'PAULDING',
-    # 'PERRY',
-    # 'PICKAWAY',
-    # 'PIKE',
-    # 'PORTAGE',
-    # 'PREBLE',
-    # 'PUTNAM',
-    # 'RICHLAND',
-    # 'ROSS',
-    # 'SANDUSKY',
-    # 'SCIOTO',
-    # 'SENECA',
-    # 'SHELBY',
-    # 'STARK',
-    # 'SUMMIT',
-    # 'TRUMBULL',
-    # 'TUSCARAWAS',
-    # 'UNION',
-    # 'VANWERT',
-    # 'VINTON',
-    # 'WARREN',
-    # 'WASHINGTON',
-    # 'WAYNE',
-    # 'WILLIAMS',
-    # 'WOOD',
-    # 'WYANDOT',
+    'ALLEN',
+    'ASHLAND',
+    'ASHTABULA',
+    'ATHENS',
+    'AUGLAIZE',
+    'BELMONT',
+    'BROWN',
+    'BUTLER',
+    'CARROLL',
+    'CHAMPAIGN',
+    'CLARK',
+    'CLERMONT',
+    'CLINTON',
+    'COLUMBIANA',
+    'COSHOCTON',
+    'CRAWFORD',
+    'CUYAHOGA',
+    'DARKE',
+    'DEFIANCE',
+    'DELAWARE',
+    'ERIE',
+    'FAIRFIELD',
+    'FAYETTE',
+    'FRANKLIN',
+    'FULTON',
+    'GALLIA',
+    'GEAUGA',
+    'GREENE',
+    'GUERNSEY',
+    'HAMILTON',
+    'HANCOCK',
+    'HARDIN',
+    'HARRISON',
+    'HENRY',
+    'HIGHLAND',
+    'HOCKING',
+    'HOLMES',
+    'HURON',
+    'JACKSON',
+    'JEFFERSON',
+    'KNOX',
+    'LAKE',
+    'LAWRENCE',
+    'LICKING',
+    'LOGAN',
+    'LORAIN',
+    'LUCAS',
+    'MADISON',
+    'MAHONING',
+    'MARION',
+    'MEDINA',
+    'MEIGS',
+    'MERCER',
+    'MIAMI',
+    'MONROE',
+    'MONTGOMERY',
+    'MORGAN',
+    'MORROW',
+    'MUSKINGUM',
+    'NOBLE',
+    'OTTAWA',
+    'PAULDING',
+    'PERRY',
+    'PICKAWAY',
+    'PIKE',
+    'PORTAGE',
+    'PREBLE',
+    'PUTNAM',
+    'RICHLAND',
+    'ROSS',
+    'SANDUSKY',
+    'SCIOTO',
+    'SENECA',
+    'SHELBY',
+    'STARK',
+    'SUMMIT',
+    'TRUMBULL',
+    'TUSCARAWAS',
+    'UNION',
+    'VANWERT',
+    'VINTON',
+    'WARREN',
+    'WASHINGTON',
+    'WAYNE',
+    'WILLIAMS',
+    'WOOD',
+    'WYANDOT',
 ]
+
 
 VOTER_COLUMNS = [
     'sos_voterid',
@@ -156,6 +159,7 @@ VOTER_COLUMNS = [
     'county',
 ]
 
+
 ELECTION_COLUMNS = [
     'id',
     'category',
@@ -163,138 +167,135 @@ ELECTION_COLUMNS = [
     'party',
 ]
 
+
 PARTICIPATION_COLUMNS = [
     'voter_id',
     'election_id',
 ]
 
-class Command(BaseCommand):
-    def handle(self, **kwargs):
 
-        message = ('\nThis command will completely wipe your database and download & parse the latest Ohio Voter File data.'
-                   '\nThe process is very slow and can take minutes or hours depending on your network and machine. Continue? (y/n): ')
+class Command(BaseCommand):
+
+    processed_elections = set()
+
+    @staticmethod
+    def download_county_data(county, destination_directory):
+        url = 'ftp://sosftp.sos.state.oh.us/free/Voter/{}.zip'.format(county)
+
+        downloaded_file = '{}/{}.zip'.format(destination_directory, county)
+        urllib.request.urlretrieve(url, downloaded_file)
+        with zipfile.ZipFile(downloaded_file, 'r') as z:
+            z.extractall(destination_directory)
+
+    @staticmethod
+    def load_county_data_into_db(county, directory_name):
+        print('Parsing and imorting {} County data...'.format(county.title()))
+
+        county_filename = '{}/{}.TXT'.format(directory_name, county)
+
+        with open(county_filename, encoding='utf-8') as input_file:
+            reader = csv.reader(input_file)
+
+            header = next(reader)
+
+            voter_stream = StringIO()
+            election_stream = StringIO()
+            participation_stream = StringIO()
+
+            voter_writer = csv.writer(voter_stream, delimiter='\t')
+            election_writer = csv.writer(election_stream, delimiter='\t')
+            participation_writer = csv.writer(participation_stream, delimiter='\t')
+
+            for row in reader:
+                this_voters_data = []
+                this_voters_elections = []
+
+                for index, column_value in enumerate(row):
+                    field_name = header[index]
+                    lower_field_name = field_name.lower()
+
+                    if hasattr(Voter, lower_field_name):  # parse Voter data
+                        this_voters_data.append(column_value)
+                    else:  # parse Election data
+                        if column_value:
+                            category_display, date_string = field_name.split('-')
+
+                            election_category = Election.CATEGORY_CHOICES_REVERSE_MAP[category_display]
+                            election_date = datetime.strptime(date_string, '%m/%d/%Y').strftime('%Y-%m-%d')
+                            election_party = column_value
+
+                            election_hashable_key = '{}.{}.{}'.format(election_category, election_date, election_party).encode('utf-8')
+                            election_id = hashlib.sha256(election_hashable_key).hexdigest()
+
+                            election_data = [election_id, election_category, election_date, election_party]
+
+                            if not election_id in Command.processed_elections:
+                                election_writer.writerow(election_data)
+                                Command.processed_elections.add(election_id)
+
+                            participation_writer.writerow([this_voters_data[0], election_id])
+
+                # I need to manually add this on since I'm inferring it
+                this_voters_data.append(county)
+                voter_writer.writerow(this_voters_data)
+
+            # Write Voters
+            voter_stream.seek(0)
+            with closing(connection.cursor()) as cursor:
+                cursor.copy_from(
+                    file=voter_stream,
+                    table='ohiovoter_voter',
+                    sep='\t',
+                    columns=VOTER_COLUMNS,
+                )
+
+            # Write Elections
+            election_stream.seek(0)
+            with closing(connection.cursor()) as cursor:
+                cursor.copy_from(
+                    file=election_stream,
+                    table='ohiovoter_election',
+                    sep='\t',
+                    columns=ELECTION_COLUMNS,
+                )
+
+            # Write Participations
+            participation_stream.seek(0)
+            with closing(connection.cursor()) as cursor:
+                cursor.copy_from(
+                    file=participation_stream,
+                    table='ohiovoter_participation',
+                    sep='\t',
+                    columns=PARTICIPATION_COLUMNS,
+                )
+
+    def handle(self, **kwargs):
+        message = ('\nThis command will completely wipe your database and '
+                   'download & parse the latest Ohio Voter File data. The '
+                   'process can take minutes or hours depending on your '
+                   'machine. Continue? (y/n): ')
 
         answer = input(message)
 
         if answer == 'y':
+            # start fresh
             management.call_command('flush', interactive=False)
             management.call_command('migrate', interactive=False)
 
-            import time
             start = time.time()
 
-            # keep this state outside of the loop since it exists between county files
-            processed_elections = set()
+            # download all the county data from the SoS website
+            print('\nDownloading County data...'
+            pool = ThreadPool(8)
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                args = [(county, tmpdirname) for county in COUNTIES]
+                pool.starmap(self.download_county_data, args)
+                pool.close()
+                pool.join()
 
-            for county in COUNTIES:
-                print('\nDownloading {} County data...'.format(county.title()))
-
-                url = 'ftp://sosftp.sos.state.oh.us/free/Voter/{}.zip'.format(county)
-
-                with tempfile.TemporaryDirectory() as tmpdirname:
-                    downloaded_file = '{}/{}.zip'.format(tmpdirname, county)
-                    urllib.request.urlretrieve(url, downloaded_file)
-                    with zipfile.ZipFile(downloaded_file, 'r') as z:
-                        z.extractall(tmpdirname)
-
-                    county_filename = '{}/{}.TXT'.format(tmpdirname, county)
-
-                    print('Parsing and loading {} County data...'.format(county.title()))
-
-                    with open(county_filename, encoding='utf-8') as input_file:
-                        reader = csv.reader(input_file)
-
-                        header = next(reader)
-
-                        voter_stream = StringIO()
-                        election_stream = StringIO()
-                        participation_stream = StringIO()
-
-                        voter_writer = csv.writer(voter_stream, delimiter='\t')
-                        election_writer = csv.writer(election_stream, delimiter='\t')
-                        participation_writer = csv.writer(participation_stream, delimiter='\t')
-
-                        c_start = time.time()
-                        print('Starting to parse CSV')
-                        for row in reader:
-                            this_voters_data = []
-                            this_voters_elections = []
-
-                            for index, column_value in enumerate(row):
-                                field_name = header[index]
-                                lower_field_name = field_name.lower()
-
-                                if hasattr(Voter, lower_field_name):  # parse Voter data
-                                    this_voters_data.append(column_value)
-                                else:  # parse Election data
-                                    if column_value:
-                                        category_display, date_string = field_name.split('-')
-
-                                        election_category = Election.CATEGORY_CHOICES_REVERSE_MAP[category_display]
-                                        election_date = datetime.strptime(date_string, '%m/%d/%Y').strftime('%Y-%m-%d')
-                                        election_party = column_value
-
-                                        election_hashable_key = '{}.{}.{}'.format(election_category, election_date, election_party).encode('utf-8')
-                                        election_id = hashlib.sha256(election_hashable_key).hexdigest()
-
-                                        election_data = [election_id, election_category, election_date, election_party]
-
-                                        if not election_id in processed_elections:
-                                            election_writer.writerow(election_data)
-                                            processed_elections.add(election_id)
-
-                                        participation_writer.writerow([this_voters_data[0], election_id])
-
-                            # I need to manually add this on since I'm inferring it
-                            this_voters_data.append(county)
-                            voter_writer.writerow(this_voters_data)
-
-                        c_end = time.time()
-                        print('Done parsing CSV')
-                        print('Parsing CSV took: {}'.format(c_end-c_start))
-
-                        v_start = time.time()
-                        print('Starting to insert Voters')
-                        voter_stream.seek(0)
-                        with closing(connection.cursor()) as cursor:
-                            cursor.copy_from(
-                                file=voter_stream,
-                                table='ohiovoter_voter',
-                                sep='\t',
-                                columns=VOTER_COLUMNS,
-                            )
-
-                        v_end = time.time()
-                        print('Done inserting Voters')
-                        print('Writing voters took: {}'.format(v_end-v_start))
-
-                        e_start = time.time()
-                        print('Starting to insert Elections')
-                        election_stream.seek(0)
-                        with closing(connection.cursor()) as cursor:
-                            cursor.copy_from(
-                                file=election_stream,
-                                table='ohiovoter_election',
-                                sep='\t',
-                                columns=ELECTION_COLUMNS,
-                            )
-                        e_end = time.time()
-                        print('Done inserting Elections')
-                        print('Writing Elections took: {}'.format(e_end-e_start))
-
-                        p_start = time.time()
-                        print('Starting to insert Participations')
-                        participation_stream.seek(0)
-                        with closing(connection.cursor()) as cursor:
-                            cursor.copy_from(
-                                file=participation_stream,
-                                table='ohiovoter_participation',
-                                sep='\t',
-                                columns=PARTICIPATION_COLUMNS,
-                            )
-                        p_end = time.time()
-                        print('Done inserting Participations')
-                        print('Writing participations took: {}'.format(p_end-p_start))
+                # Parse and load it all into the database
+                for county in COUNTIES:
+                    self.load_county_data_into_db(county, tmpdirname)
 
             print('\nDone!')
 

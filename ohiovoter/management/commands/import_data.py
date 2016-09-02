442bd2836bde4d4478af824485154109b903e3c0
Matt Hodges <hodgesmr1@gmail.com>
2016-09-02 19:26:59 -0400

    typo fix

--- ohiovoter/management/commands/import_data.py
+++ ohiovoter/management/commands/import_data.py
@@ -285,7 +285,7 @@ class Command(BaseCommand):
             with tempfile.TemporaryDirectory() as tmpdirname:
                 num_cpus = cpu_count()
 
-                print('Starting {} worker processes...').format(num_cpus)
+                print('Starting {} worker processes...'.format(num_cpus))
                 pool = Pool(num_cpus)
 
                 args = [(county, tmpdirname) for county in COUNTIES]
